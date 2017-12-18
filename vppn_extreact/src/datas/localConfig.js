f7bc89f10a70c544d0e791f4d3325970d2df5521
liuyaoao <623578381@qq.com>
2017-12-18 09:37:52 +0800

    delete a project about vppn_extreact

--- vppn_extreact/src/datas/localConfig.js
+++
@@ -1,9 +0,0 @@
-export const vPathPacksData ={
-  China2World:[
-    'google', 'twitter', 'wenxuecity', 'blogshot', 'twimg', 'blog', 'nytimes', 'wsj', 'bloomberg',
-    'wordpress', 'line', 'vpn', 'dropbox', 'gmail', 'businessweek', 'ytimg', 't.co'
-  ],
-  World2China:[
-    'baidu.com', 'zhihu.com', 'weibo.com','youku.com'
-  ],
-}
--- vppn_extreact/src/reactWM_lib/index.js
+++
@@ -1,9 +0,0 @@
-// module.exports = require('./views/manager');
-
-import Manager from './views/manager';
-import Taskbar from './views/taskbar';
-
-export default {
-  Manager:Manager,
-  Taskbar:Taskbar
-}
